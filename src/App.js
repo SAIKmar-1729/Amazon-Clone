c4da01ce6dfe755192bceff0e8bc903d9b8afdb6
SAIKmar-1729 <83260137+SAIKmar-1729@users.noreply.github.com>
2022-12-31 08:35:24 +0530

    Created HomePage and CheckOut without Any functionality.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Home from "./Home";
+import { Routes, Route } from "react-router-dom"
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+      <Routes>
+        <Route path="/checkout"
+          element={<div>
+            <Checkout/>
+          </div>}>
+        </Route>
+        <Route path="/"
+          element={<div>
+            <Home />
+          </div>} >
+        </Route>
+      </Routes>
     </div>
+
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCOWtRZ71amYukRQCa2iU4kRwfhv2lGQTg",
+    authDomain: "clone-730e1.firebaseapp.com",
+    projectId: "clone-730e1",
+    storageBucket: "clone-730e1.appspot.com",
+    messagingSenderId: "628831261667",
+    appId: "1:628831261667:web:76084b2611fe2680d106d6",
+    measurementId: "G-1YESCSM52W"
+  };
